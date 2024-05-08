6bd8f208368b8be24c9633c7426963c84a6006aa
Leonardo Ramírez Gutiérrez <150188420+LeeonDeChino@users.noreply.github.com>
2024-05-07 19:51:55 -0600

    3 tipos de enemigos
    Agregué los distintos tipos de enemigos: Small, Medium, Big.

--- Assets/Scripts/Enemies/EnemyMovement.cs
+++ Assets/Scripts/Enemies/EnemyMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
@@ -7,12 +8,28 @@ public class EnemyMovement : MonoBehaviour
 
     public Transform[] waypoints;
     public int targetPoint;
-    public float speed;
-    public float toleranceDistance = 0.3f;
+    public float toleranceDistance = 0.5f;
 
-    private void Start()
+    private float smallSpeed = 8f;
+    private float medSpeed = 5f;
+    private float bigSpeed = 3f;
+
+    private int smallDmg = 1;
+    private int medDmg = 2;
+    private int bigDmg = 3;
+
+    private int smallHP = 1;
+    private int medHP = 2;
+    private int bigHP = 3;
+
+    [SerializeField] private float enemySpeed;
+    private int enemyDmg;
+    private int enemyHP;
+
+    private void Start() //Cambiar por OnEnable cuando ya esté configurado el spawner
     {
         targetPoint = 0;
+        SetEnemyType();
 
     }
 
@@ -29,7 +46,7 @@ public class EnemyMovement : MonoBehaviour
         float dt = Time.deltaTime;
         Vector3 movementDirection = (waypoints[targetPoint].position - transform.position).normalized;
         transform.forward = movementDirection;
-        transform.Translate(movementDirection * dt * speed, Space.World);
+        transform.Translate(movementDirection * dt * enemySpeed, Space.World);
         float distanceToWp = Vector3.Distance(transform.position, waypoints[targetPoint].position);
         if (distanceToWp <= toleranceDistance)
         {
@@ -38,6 +55,36 @@ public class EnemyMovement : MonoBehaviour
             {
                 gameObject.SetActive(false);
             }
+        }
+    }
+
+    private void SetEnemyType()
+    {
+        if (this.tag == "EnemySmall")
+        {
+            enemySpeed = smallSpeed;
+            enemyDmg = smallDmg;
+            enemyHP = smallHP;
+            Debug.Log("Small enemy spawned");
+        }
+        else if (this.tag == "EnemyMedium")
+        {
+            enemySpeed = medSpeed;
+            enemyDmg = medDmg;
+            enemyHP = medHP;
+            Debug.Log("Medium enemy spawned");
+        }
+        else if (this.tag == "EnemyBig")
+        {
+            enemySpeed = bigSpeed;
+            enemyDmg = bigDmg;
+            enemyHP = bigHP;
+            Debug.Log("Big enemy spawned");
+        }
+
+        else
+        {
+            Debug.Log("Error: Enemy Tag not found");
         }
     }
 }
