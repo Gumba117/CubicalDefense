824abcc26e87dd4234ee65aaa6451b41b3c67bbe
Jorge Palacios <cdmx2637@amerike.edu.mx>
2024-05-19 00:28:08 -0600

    Escenarios modo infinito
    Agregando cambios a los escenarios en la escena de Modo infinito

--- Assets/Scripts/UI/MenuManager.cs
+++ Assets/Scripts/UI/MenuManager.cs
@@ -8,7 +8,7 @@ public class MenuManager : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("Level1");
+        SceneManager.LoadScene("ModoInfinito");
     }
     public void QuitGame()
     {
